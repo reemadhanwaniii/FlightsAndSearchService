50247d84b59a21d18fe4bd2dac11d93b0005f331
Reema Dhanwani <reemadhanwani1234@gmail.com>
2023-03-10 14:27:26 +0530

    Completed City Repository

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+
 const { PORT } = require('./config/serverConfig');
+
+
 
 const setupAndStartServer = async () => {
     const app = express();
@@ -8,7 +11,7 @@ const setupAndStartServer = async () => {
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended : true}));
 
-    app.listen(PORT,() => {
+    app.listen(PORT,async () => {
         console.log(`Server Started at ${PORT}`);
     })
 }
